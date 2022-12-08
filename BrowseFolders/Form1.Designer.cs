a012e0e6931b4ed3b087b288e3e69e97d157216c
Tallent <ctallent@corelogic.com>
2022-12-08 09:53:42 -0600

    Add project files.

---
+++ BrowseFolders/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace BrowseFolders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialogDesktop = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialogMyComputer = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonDesktop = new System.Windows.Forms.Button();
+            this.buttonMyComputer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialogMyComputer
+            // 
+            this.folderBrowserDialogMyComputer.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // buttonDesktop
+            // 
+            this.buttonDesktop.Location = new System.Drawing.Point(107, 36);
+            this.buttonDesktop.Name = "buttonDesktop";
+            this.buttonDesktop.Size = new System.Drawing.Size(119, 23);
+            this.buttonDesktop.TabIndex = 0;
+            this.buttonDesktop.Text = "Desktop";
+            this.buttonDesktop.UseVisualStyleBackColor = true;
+            this.buttonDesktop.Click += new System.EventHandler(this.buttonDesktop_Click);
+            // 
+            // buttonMyComputer
+            // 
+            this.buttonMyComputer.Location = new System.Drawing.Point(107, 79);
+            this.buttonMyComputer.Name = "buttonMyComputer";
+            this.buttonMyComputer.Size = new System.Drawing.Size(119, 23);
+            this.buttonMyComputer.TabIndex = 1;
+            this.buttonMyComputer.Text = "My Computer";
+            this.buttonMyComputer.UseVisualStyleBackColor = true;
+            this.buttonMyComputer.Click += new System.EventHandler(this.buttonMyComputer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 138);
+            this.Controls.Add(this.buttonMyComputer);
+            this.Controls.Add(this.buttonDesktop);
+            this.Name = "Form1";
+            this.Text = "Browse Test App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogDesktop;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogMyComputer;
+        private System.Windows.Forms.Button buttonDesktop;
+        private System.Windows.Forms.Button buttonMyComputer;
+    }
+}
+
---
+++ BrowseFolders/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowseFolders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDesktop_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialogDesktop.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(folderBrowserDialogDesktop.SelectedPath, "Selected Folder");
+            }
+        }
+
+        private void buttonMyComputer_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialogMyComputer.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(folderBrowserDialogMyComputer.SelectedPath, "Selected Folder");
+            }
+        }
+    }
+}
